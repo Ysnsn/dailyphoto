0b555a9695106d87a93ea75c9c3ff96184758fce
阳光小饺子 <yangsky.vip@gmail.com>
2021-05-15 14:50:31 +0800

    Update zuimeiawallpaper.js

--- zuimeiawallpaper.js
+++ zuimeiawallpaper.js
@@ -1,6 +1,8 @@
 const updataImg = require('./updataImg');
 const fs = require("fs");
 const axios = require("axios");
+const chatid = process.env.chatid;
+const botapi = process.env.botapi;
 
 function image() {
 
@@ -36,6 +38,25 @@ function image() {
 
 }
 
+function tgbot(imgg,text) {
+    return new Promise(async (resolve) => {
+        try {
+            let url = `https://api.telegram.org/bot${botapi}/sendMessage?chat_id=${chatid}`;
+            let data =`parse_mode=MarkdownV2&text=[ ](${imgg}) ${text}`
+            let res = await axios.post(url,data);
+            if (res.data.ok) {
+                console.log("Tg：发送成功");
+            } else {
+                console.log("Tg：发送失败!");
+                console.log(res.data);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+        resolve();
+    });
+}
+ 
 function task() {
 
   return new Promise(async (resolve) => {
@@ -54,6 +75,7 @@ function task() {
         })
 
       updataImg(a.thumb,a.time+"  " +a.title)
+      tgbot(a.thumb,a.title)
 
     } catch (err) {
 
